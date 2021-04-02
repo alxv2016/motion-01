a2c45533c6e1b3d2a38670f8d6324ab39eeecb78
Alex Vong <avong2011@gmail.com>
2021-04-02 15:32:09 -0400

    feat: added svg animation

--- src/app/components/art-frame/art-frame.component.ts
+++ src/app/components/art-frame/art-frame.component.ts
@@ -1,11 +1,12 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, HostBinding, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-art-frame',
+  selector: 'c-art-frame',
   templateUrl: './art-frame.component.html',
   styleUrls: ['./art-frame.component.scss'],
 })
 export class ArtFrameComponent implements OnInit {
+  @HostBinding('class') class = 'c-art-frame';
   constructor() {}
 
   ngOnInit(): void {}
--- src/app/components/svg-object/svg-object.component.ts
+++ src/app/components/svg-object/svg-object.component.ts
@@ -1,12 +1,48 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, HostBinding, OnInit, QueryList, ViewChildren} from '@angular/core';
+import {gsap} from 'gsap';
+import {ScrollTrigger} from 'gsap/ScrollTrigger';
 
 @Component({
-  selector: 'app-svg-object',
+  selector: 'c-svg-object',
   templateUrl: './svg-object.component.html',
   styleUrls: ['./svg-object.component.scss'],
 })
-export class SvgObjectComponent implements OnInit {
-  constructor() {}
+export class SvgObjectComponent implements OnInit, AfterViewInit {
+  @HostBinding('class') class = 'c-svg-object';
+  @ViewChildren('circle', {read: ElementRef}) circle!: QueryList<ElementRef>;
+  constructor() {
+    gsap.registerPlugin(ScrollTrigger);
+  }
 
   ngOnInit(): void {}
+
+  ngAfterViewInit(): void {
+    const circles = this.circle.map((cir) => cir.nativeElement);
+    const staggering = gsap.timeline({
+      defaults: {
+        repeat: -1,
+        yoyo: true,
+        yoyoEase: true,
+        duration: 1.75,
+        transformOrigin: '50%',
+        ease: 'back',
+        stagger: {
+          grid: 'auto',
+          amount: 2.75,
+          from: 'center',
+          //ease: 'power2.in',
+        },
+      },
+    });
+
+    staggering.fromTo(
+      circles,
+      {
+        scale: 0.125,
+      },
+      {
+        scale: 0.9,
+      }
+    );
+  }
 }
